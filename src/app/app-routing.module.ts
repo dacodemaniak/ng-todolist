3db43fa9709c435266d08de8d838a294aace44c4
Jean-Luc <jean-luc.a@ideafactory.fr>
2020-12-09 12:28:47 +0100

    Add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddTodoComponent } from './pages/add-todo/add-todo.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'add',
+    component: AddTodoComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { FooterComponent } from './components/footer/footer.component';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
 import { FilterBarComponent } from './components/filter-bar/filter-bar.component';
 import { AddTodoComponent } from './pages/add-todo/add-todo.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { AddTodoComponent } from './pages/add-todo/add-todo.component';
     FooterComponent,
     TodoListComponent,
     FilterBarComponent,
-    AddTodoComponent
+    AddTodoComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
